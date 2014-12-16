5885081b4f656b29d4a4d32451056e2373707aeb
OhohLeo <lmartin.leo@gmail.com>
2014-12-16 09:23:53 +0100

    16/12/2014 Leo  * 2ème version du site  * à finir : bordure, onglet Paroles, onglet Membre & Page d'accueil

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ WEB-DiveBar
 
 Dive Bar website sources
 
+16/12/2014 Leo
+ * 2ème version du site
+ * à finir : bordure, onglet Paroles, onglet Membre & Page d'accueil
+
 26/11/2014 Leo
  * première version brute du site
  * à finir : onglet Paroles & Photos
--- js/divebar.js
+++ js/divebar.js
@@ -5,19 +5,28 @@ var $menu    = $("div#menu");
 var $menus   = $("div.menu");
 var $li_menu = $("li.menu");
 var $hr_menu = $("hr#menu");
+var $p_random = $("p.random");
+var $random_msg = function() {
+    var $random = Math.floor(Math.random() * $p_random.length);
+    $p_random.hide()
+	     .eq($random).show();
+    $("div#message").show();
+};
 
 var $data, $last_menu, $is_accueil = 1;
 
 // mise en place du slider
 $(window).load(function() {
-
     // on gère la taille de la fenêtre
     var $width = $(window).width();
     var $height = parseInt($width * 0.36);
 
     $accueil.css({'height': $height,'width': $width});
     $("li.bg").css({'height': $height,'width': $width});
-    $("img#logo_banner").css({'height': $height / 3,'width': $width / 3});
+    var $logos = $("img.logo");
+    var $logo_generic = $("img#generic");
+
+    $logos.css({'height': $height / 3,'width': $width / 3});
 
     var $slide = $("div.banner").unslider({
         speed: 1000,
@@ -26,99 +35,153 @@ $(window).load(function() {
         fluid: false,
     });
 
-	<!-- on démarre le slider -->
-	$data = $slide.data("unslider");
+    // on démarre le slider
+    $data = $slide.data("unslider");
     $data.start();
-});
 
-var $handle_color = function ($background, $border) {
-    $li_menu.animate({'background-color': $background}, 2000);
-    $body.animate({'background-color': $background}, 2000);
-    $hr_menu.animate({'background-color': $background}, 2000);
-}
+    // on affiche le message aléatoire
+    $random_msg();
 
-$(".start").on("click", function() {
-    $is_accueil = 1;
-    $last_menu = null;
-    $handle_color('#3F0000', '#EDC190');
-    $accueil.slideDown();
-    $menu.hide();
-    $menus.hide();
-    $data.start();
-    $accueil_menu.show();
-});
-
-$li_menu.on("click", function() {
-    var $name = $(this).attr('id');
-    var $new_menu = $("div#" + $name);
-
-    // on ne change pas le menu
-    if ($new_menu.is($last_menu) || $name === 'accueil') {
-        return;
+    var $handle_color = function ($background, $border) {
+	$li_menu.animate({'background-color': $background}, 2000);
+	$body.animate({'background-color': $background}, 2000);
+	$hr_menu.animate({'background-color': $background}, 2000);
     }
 
-    // on gère le cas particulier de l'accueil
-    if ($is_accueil) {
-	$accueil_menu.hide();
-	$data.stop();
-	$accueil.slideUp();
-	$is_accueil = 0;
-    }
-
-    //on gère le cas particulier des couleurs
-    if ($name === "videos" || $name === "photos") {
-	$handle_color("#000000", "#000000");
-    } else if ($name === "members" || $name === "lyrics") {
-	$handle_color("#080224", "#080224");
-    } else {
-	$handle_color("#3F0000", "#3F0000");
-    }
-
-	<!-- on gère le cas particulier des logos -->
-	var $logo = $("img#logo_banner");
-    if ($name === "photos") {
-	$logo.attr("src", "img/photos/logo.jpg");
-    } else if ($name === "contact") {
-	$logo.attr("src", "img/contact/logo.png");
-    } else {
-	$logo.attr("src", "img/accueil/logo.png");
-    }
-    $menu.show();
-
-    if ($last_menu != null) {
-	$last_menu.slideUp(1000);
-    }
-
-    $new_menu.slideDown(1000);
-    $last_menu = $new_menu;
-});
-
-var $is_already_enter;
-var $members = $("li.members");
-var $members_to_hide = $members.children().not('h2');
-
-$members_to_hide.hide();
-
-$members.on("mouseenter", function() {
-    if ($(this).is($is_already_enter)) {
-	return;
-    }
-
-    if ($is_already_enter != null) {
-	$is_already_enter.css({ "background-image": "" });
-	$is_already_enter.animate({ "padding-bottom": "0px" }, 500);
-    }
-
-    $is_already_enter = $(this);
-
-    var $id = $is_already_enter.attr('id');
-
-    $is_already_enter.css({
-	"background-image": "url('img/members/" + $id + ".jpg')"
+    $(".start").on("click", function() {
+	$is_accueil = 1;
+	$last_menu = null;
+	$handle_color('#3F0000', '#EDC190');
+	$accueil.slideDown();
+	$menu.hide();
+	$menus.hide();
+	$data.start();
+	$accueil_menu.show();
     });
 
-    $is_already_enter.animate({ "padding-bottom": "415px" }, 500);
+    var $load_photos = function($div_photos) {
+	$div_photos.nanoGallery({
+	    thumbnailWidth: 'auto',
+	    thumbnailHeight: 300,
+	    kind: 'picasa',
+	    userID: '112854609172007422557',
+	    thumbnailAlignment: 'justified',
+	    thumbnailHoverEffect: 'borderLighter,labelAppear75',
+	    thumbnailGutterWidth: 0,
+	    thumbnailGutterHeight: 0,
+	    thumbnailLabel: {
+		display: true,
+		position: 'overImageOnMiddle',
+		hideIcons: true,
+		align: 'center',
+	    },
+	    i18n: {
+		thumbnailLabelItemsCountPart1: '',
+		thumbnailImageDescription: 'click to open'
+            },
+	    colorScheme: 'none',
+	    galleryToolbarWidthAligned: true,
+	    galleryToolbarHideIcons: true,
+	    galleryFullpageButton: true,
+	});
+    }
 
+    $li_menu.on("click", function() {
+	var $name = $(this).attr('id');
+	var $new_menu = $("div#" + $name);
+
+	$logos.hide();
+	var $logo_actual = $("img#" + $name);
+	if ($logo_actual.length) {
+	    $logo_actual.show();
+	} else {
+	    $logo_generic.show();
+	}
+
+	// on ne change pas le menu
+	if ($new_menu.is($last_menu) || $name === 'accueil') {
+            return;
+	}
+
+	// on gère le cas particulier de l'accueil
+	if ($is_accueil) {
+	    $random_msg();
+	    $accueil_menu.hide();
+	    $data.stop();
+	    $accueil.slideUp();
+	    $is_accueil = 0;
+	}
+
+	//on gère le cas particulier des couleurs
+	if ($name === "videos" || $name === "photos") {
+	    $handle_color("#000000", "#000000");
+	} else if ($name === "members" || $name === "lyrics") {
+	    $handle_color("#080224", "#080224");
+	} else {
+	    $handle_color("#3F0000", "#3F0000");
+	}
+
+	// on gère le cas particulier des photos
+	if ($load_photos != null && $name === "photos") {
+	    $load_photos($new_menu);
+	    $load_photos = null;
+	}
+
+	$menu.show();
+
+	if ($last_menu != null) {
+	    $last_menu.slideUp(1000);
+	}
+
+	$new_menu.slideDown(1000);
+	$last_menu = $new_menu;
+    });
+
+    // dans l'onglet members : on diminue la taille du titre pour
+    // qu'il n'empiète pas sur l'image
+    if ($width < 500) {
+	$("h2.name").css({
+	    "font-size": "15px",
+	});
+
+	$("div.biography").css({
+	    "position": "relative",
+	    "width": "80%",
+	});
+    }
+
+    // on s'occupe de l'animation des membres
+    var $is_already_enter;
+    var $members = $("li.members");
+    var $members_to_hide = $members.children().not('h2');
+    var $height_members = parseInt($width * 0.5);
+    console.log($height_members);
+
+    // par défault tous les membres sont cachés, sauf le nom du membre
     $members_to_hide.hide();
-    $is_already_enter.children().fadeIn(500);
+
+    $members.on("click", function() {
+	if ($(this).is($is_already_enter)) {
+	    return;
+	}
+
+	if ($is_already_enter != null) {
+	    $is_already_enter.css({ "background-image": "" });
+	    $is_already_enter.animate({ "padding-bottom": "0" }, 500);
+	}
+
+	$is_already_enter = $(this);
+
+	var $id = $is_already_enter.attr('id');
+
+	$is_already_enter.css({
+	    "background-image": "url('img/members/" + $id + ".jpg')",
+	    "background-size": $width + "px " + $height_members + "px",
+	});
+
+	$is_already_enter.animate({ "padding-bottom": "150px" }, 500);
+
+	$members_to_hide.hide();
+	$is_already_enter.children().fadeIn(500);
+    });
 });
